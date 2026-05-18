fc015b45c8508610206065f14d3c0017c40a6851
adrien caubel <adriencaubel@macbook-pro-de-adrien.home>
2026-05-18 14:29:33 +0200

    correction entoty

--- src/main/java/fr/school/library/entity/Author.java
+++ src/main/java/fr/school/library/entity/Author.java
@@ -1,10 +1,6 @@
 package fr.school.library.entity;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +10,7 @@ import java.util.List;
 public class Author {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
